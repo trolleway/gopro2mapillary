19aa1c01044cb482c634a6a95bacbbb934e46923
Artem Svetlov <trolleway@yandex.ru>
2019-07-17 16:48:40 +0300

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,18 @@
+#validate
+#check if any mp4 in folder
+
+#cycle
+    #export data stream from mp4
+
+    #convert data to gpx
+
+    #get lenght of time of gpx track
+
+    #get length of video
+
+    #calc video speed rate (x1, x5, x10, x15, x30)
+
+    #if video speed not x1 - slow down video using ffmpeg
+    #later mapillary video_process will operate as video in standart speed
+
+    #call video_process
