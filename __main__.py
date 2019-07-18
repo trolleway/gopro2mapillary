549266cb46d6e40951a0694844da635765f06818
Artem Svetlov <trolleway@yandex.ru>
2019-07-19 00:59:18 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,23 @@
+
+import gpxpy
+import gpxpy.gpx
+
+from datetime import datetime
+
+def get_gpx_duration(gpx_filename):
+    gpx_file = open(gpx_filename, 'r')
+    gpx = gpxpy.parse(gpx_file)
+
+    frist_point = None
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                if frist_point is None: frist_point = point
+    last_point = point
+
+    difference = (last_point.time - frist_point.time).total_seconds()
+    return(int(difference))
+
 #validate
 #check if any mp4 in folder
 
